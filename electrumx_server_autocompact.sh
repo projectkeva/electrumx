ee839bbac9032bf48a89747d1530219bd4c702a8
K <k@M4.asus-us>
2026-05-07 20:45:46 -1000

    Allow autocompact Python override

--- electrumx_server_autocompact.sh
+++ electrumx_server_autocompact.sh
@@ -2,11 +2,12 @@
 set -u
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR" || exit 1
+ELECTRUMX_PYTHON="${ELECTRUMX_PYTHON:-python3}"
 
-while "$SCRIPT_DIR/electrumx_server"; exitcode=$? && test $exitcode -eq 65;
+while "$ELECTRUMX_PYTHON" "$SCRIPT_DIR/electrumx_server"; exitcode=$? && test $exitcode -eq 65;
 do
     echo Attempting automatic electrumx_compact_history.
-    "$SCRIPT_DIR/electrumx_compact_history"
+    "$ELECTRUMX_PYTHON" "$SCRIPT_DIR/electrumx_compact_history"
     # exit on compaction failure
     if test $? -ne 0
     then
