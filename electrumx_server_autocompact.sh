023c54364ec631e8cf479801330552a4a495b468
K <k@M4.asus-us>
2026-05-07 20:03:46 -1000

    Make autocompact script path independent

--- electrumx_server_autocompact.sh
+++ electrumx_server_autocompact.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-while ./electrumx_server; exitcode=$? && test $exitcode -eq 65;
+set -u
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$SCRIPT_DIR" || exit 1
+
+while "$SCRIPT_DIR/electrumx_server"; exitcode=$? && test $exitcode -eq 65;
 do
     echo Attempting automatic electrumx_compact_history.
-    ./electrumx_compact_history
+    "$SCRIPT_DIR/electrumx_compact_history"
     # exit on compaction failure
     if test $? -ne 0
     then
